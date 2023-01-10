ca5b54a00542412825e3c0ae5b6011dd56623766
Brice Veyna <bricev99@gmail.com>
2023-01-09 17:07:45 -0800

    Simplify structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import IndustrialMachining from './components/sections/IndustrialMachining';
+import ProductDevelopment from './components/sections/ProductDevelopment';
+import ArtAndDesign from './components/sections/ArtAndDesign';
+import DesignEngineering from './components/sections/DesignEngineering';
 import './App.css';
 
-const App = () => <PortfolioContainer />;
-
-export default App;
+export default function App() {
+    return (
+        <div id='background'>
+            <Navigation />
+            <Header />
+            <DesignEngineering />
+            <IndustrialMachining />
+            <ProductDevelopment />
+            <ArtAndDesign />
+            <Footer />
+        </div>
+    );
+}
